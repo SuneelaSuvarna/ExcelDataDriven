58dc6c928f811dfcc7f65ce007dd661b3de06695
suneela <suneela@Suneela-MBP.local>
2021-06-13 11:25:55 +0530

    Added more code

--- src/test/java/ExcelAutomation.java
+++ src/test/java/ExcelAutomation.java
@@ -1,4 +1,5 @@
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Iterator;
 
 import org.apache.poi.ss.usermodel.Cell;
@@ -8,40 +9,59 @@ import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
 public class ExcelAutomation {
 
-	
-	//Identify the TestCases coulmn by scanning entire first row
-	//Once column is identified then scan the first column to identify the "Purchase" testcase
-	//Once Purchase is identified then fetch all the data of that row and feed into test
+	// Identify the TestCases coulmn by scanning entire first row
+	// Once column is identified then scan the first column to identify the
+	// "Purchase" testcase
+	// Once Purchase is identified then fetch all the data of that row and feed into
+	// test
 	public static void main(String[] args) throws IOException {
 
 		// FileInputStream fis= new
 		// FileInputStream("/Volumes/Development/Automation/ExcelDataDriven/ExcelTestData.xlsx");
 		// XSSFWorkbook workBook=new XSSFWorkbook(fis);
-
+		int index = 0;
 		XSSFWorkbook workBook = new XSSFWorkbook("/Volumes/Development/Automation/ExcelDataDriven/ExcelTestData.xlsx");
+		ArrayList<String>al=new ArrayList<String>();
 
 		int sheetCount = workBook.getNumberOfSheets();
 
 		for (int i = 0; i < sheetCount; i++) {
 			if (workBook.getSheetAt(i).getSheetName().equalsIgnoreCase("TestData")) {
 				XSSFSheet sheet = workBook.getSheetAt(i);
+
+				Iterator<Row> row = sheet.iterator();
+
+				Row firstRow = row.next();
+
+				Iterator<Cell> cell = firstRow.cellIterator();
+
+				while (cell.hasNext()) {
+					Cell value = cell.next();
+					if (value.getStringCellValue().equalsIgnoreCase("TestCases")) {
+						index = value.getColumnIndex();
+					}
+					
+				}
 				
-			Iterator<Row>row=	sheet.iterator();
+				while(row.hasNext())
+				{
+					Row r= row.next();
+					String rowValue=r.getCell(index).getStringCellValue();
+					if(rowValue.equalsIgnoreCase("Purchase"))
+					{
+						Iterator<Cell>c=r.cellIterator();
+						while(c.hasNext())
+						{
+							Cell c1=c.next();
+							al.add(c1.getStringCellValue());
+							
+						}
+					}
+					
+				}
 				
-			Row firstRow=row.next();
-			
-		Iterator <Cell> cell=	firstRow.cellIterator();
-			
-while(cell.hasNext())
-{
-	Cell value=cell.next();
-	if(value.getStringCellValue().equalsIgnoreCase("TestCases"))
-	{
-		//	
-	}
-}
-			
-			
+				System.out.println(al);
+
 			}
 		}
 
